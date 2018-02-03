47882d5018ec47a2cc1367652e00ee01d44549c9
Mina Gad <minashehatacs@gmail.com>
2018-02-03 18:18:05 +0200

    Initial Commit

---
+++ TwitterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterApp
+//
+//  Created by Mina Gad on 2/3/18.
+//  Copyright © 2018 Mina Gad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
